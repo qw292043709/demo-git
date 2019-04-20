8f6337884325ac544dc08a659b3fbcc85bddd83e
29204309@qq.com <292043709@qq.com>
2019-04-20 20:38:29 +0800

    123123

---
+++ src/main/java/Test1.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test1 {
+
+    public static void main(String[] args){
+        Map<String,Object> map = new HashMap<String, Object>();
+        map.put("a",1);
+        map.put("b",2);
+        map.put("c",3);
+        map.put("d",4);
+        map.put("a",5);
+
+        for (int i=0;i<5;i++){
+            if(i==4){
+                System.out.println("测试循环！");
+                break;
+            }
+        }
+
+        String b = null ;
+        System.out.println(b.length());
+
+
+    }
+
+}
